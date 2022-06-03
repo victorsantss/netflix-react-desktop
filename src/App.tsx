88126c3ee3f9db2e13092e132bbe07c39be31b5c
Victor Santos <victords895@gmail.com>
2022-06-03 16:16:19 -0300

    Movies list screen create and route paths add

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import Login from "./screens/login/login.screen";
 import GlobalStyles from "./themes/main/global-styles";
 import { ThemeProvider } from "styled-components";
 import theme from "./themes/main/theme";
+import { Route, Routes } from "react-router-dom";
+import MoviesList from "./screens/movies-list/movies-list.screen";
+import { MOVIES_LIST_URL } from "./screens/movies-list/movies-list.type";
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
-        <Login />
+        <Routes>
+          <Route element={<Login />} path="/login"></Route>
+          <Route element={<MoviesList />} path={MOVIES_LIST_URL}></Route>
+        </Routes>
       </ThemeProvider>
     </>
   );
