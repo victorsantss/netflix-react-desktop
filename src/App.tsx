5c7d843dc8d1c6630bf2b3f36448e5ae222392ff
Victor Santos <victords895@gmail.com>
2022-06-03 16:27:55 -0300

    New type file for login

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import theme from "./themes/main/theme";
 import { Route, Routes } from "react-router-dom";
 import MoviesList from "./screens/movies-list/movies-list.screen";
 import { MOVIES_LIST_URL } from "./screens/movies-list/movies-list.type";
+import { LOGIN_URL } from "./screens/login/login.type";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <GlobalStyles />
       <ThemeProvider theme={theme}>
         <Routes>
-          <Route element={<Login />} path="/login"></Route>
+          <Route element={<Login />} path={LOGIN_URL}></Route>
           <Route element={<MoviesList />} path={MOVIES_LIST_URL}></Route>
         </Routes>
       </ThemeProvider>
