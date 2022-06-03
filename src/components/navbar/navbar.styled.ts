d2abcd352e555af2067eef22dbf4daece8309c1c
Victor Santos <victords895@gmail.com>
2022-06-03 19:33:58 -0300

    Navbar Create

---
+++ src/components/navbar/navbar.styled.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  width: 100%;
+  position: absolute;
+  background: transparent;
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem 0 2rem;
+  z-index: 10;
+  img {
+    width: 14rem;
+    height: 8rem;
+  }
+  a {
+    height: 36px;
+  }
+  button {
+    width: 5rem;
+  }
+`;
