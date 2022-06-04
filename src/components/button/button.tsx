4411487f64004194d79ff6ccf8fe03d26ee84a2d
Victor Santos <victords895@gmail.com>
2022-06-03 23:43:42 -0300

    Types removed

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,9 +1,5 @@
 import { ButtonStyled } from "./button.styled";
 
-// interface ButtonProps {
-//   handleSubmit: (data: any) => void;
-// }
-
 export default function Button(props: any) {
   return <ButtonStyled {...props} />;
 }
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,12 +1,5 @@
 import { InputStyled } from "./input.styled";
 
-// interface InputProps {
-//   type: string;
-//   email: string;
-//   placeholder: string;
-//   handleChange: () => void;
-// }
-
 export default function Input(props: any) {
   return <InputStyled {...props} />;
 }
