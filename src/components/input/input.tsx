32c58aea33709ffccee0fe396579b455caeb0cc7
Victor Santos <victords895@gmail.com>
2022-06-21 22:57:05 -0300

    project refactor using types

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,5 +1,14 @@
+import React from "react";
 import { InputStyled } from "./input.styled";
+import { Props } from "./input.type";
 
-export default function Input(props: any) {
-  return <InputStyled {...props} />;
+export default function Input({ name, type, placeholder, onChange }: Props) {
+  return (
+    <InputStyled
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+  );
 }
--- src/screens/movies-list/movies-list.screen.tsx
+++ src/screens/movies-list/movies-list.screen.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function MoviesList() {
   return <h1>Hello World!</h1>;
 }
